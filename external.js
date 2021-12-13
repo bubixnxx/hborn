eca2aab107e9846ffee93726019e723dd5438a78
MegaWAT <gokuvz@wp.pl>
2021-12-13 23:31:35 +0100

    Add files via upload

--- external.js
+++ external.js
@@ -330,7 +330,7 @@ $(".BOT_box .BOT_calc_Lvl").click(() => {
 });
 console.clear();
 console.log('%cSkrypt został poprawnie załadowany!','color: #fff; width:100%; background: #05d30f; padding: 5px; font-size:20px;');
-$('body script:last-child').remove();
+$("script").last().remove();
 
 const bot_auth = [448639,457638,433273,464892,468932,442405,421729,432743];
 
